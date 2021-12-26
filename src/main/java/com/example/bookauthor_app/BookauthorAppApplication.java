6abb16ef988eb5e1d1eb0a177a54224c2e39d701
Rabin Gurung <leongurung029@icloud.com>
2021-12-26 13:18:52 +0545

    Initial commit - Empty Project

---
+++ src/main/java/com/example/bookauthor_app/BookauthorAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookauthor_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookauthorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookauthorAppApplication.class, args);
+    }
+
+}
