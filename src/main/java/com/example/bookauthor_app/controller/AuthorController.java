09e78f236c9277710d639e4e1504915c659d4a6f
Rabin Gurung <leongurung029@icloud.com>
2021-12-27 16:20:33 +0545

    operation specification added

--- src/main/java/com/example/bookauthor_app/controller/AuthorController.java
+++ src/main/java/com/example/bookauthor_app/controller/AuthorController.java
@@ -6,6 +6,10 @@ import com.example.bookauthor_app.projections.AuthorBookCount;
 import com.example.bookauthor_app.exception.DefaultResponse;
 import com.example.bookauthor_app.exception.NotFoundException;
 import com.example.bookauthor_app.service.author.AuthorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +24,7 @@ import java.util.List;
 public class AuthorController implements BaseControllerInterface<AuthorDTO,Long> {
     private final AuthorService authorService;
 
+    @Operation(summary = "Add author with book written by them, Multiple")
     @PostMapping("/add")
     @Override
     public ResponseEntity<DefaultResponse> add(@Valid @RequestBody AuthorDTO authorDTO) {
@@ -27,12 +32,14 @@ public class AuthorController implements BaseControllerInterface<AuthorDTO,Long>
         return ResponseEntity.ok().body(new DefaultResponse(HttpStatus.OK.toString()));
     }
 
+    @Operation(summary = "Get All the authors. Note this also returns nested child.")
     @GetMapping("/getAll")
     @Override
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok(authorService.getAll());
     }
 
+    @Operation(summary = "Return author details by author id")
     @GetMapping("/findOne/{author_id}")
     @Override
     public ResponseEntity<AuthorDTO> findOne(@PathVariable(value = "author_id") Long authorId) throws NotFoundException {
@@ -44,6 +51,7 @@ public class AuthorController implements BaseControllerInterface<AuthorDTO,Long>
         }
     }
 
+    @Operation(summary = "update author, requires id, updates only authorName")
     @PutMapping("/update/{author_id}")
     @Override
     public ResponseEntity<?> update(@PathVariable(value = "author_id") Long id, @Valid @RequestBody AuthorDTO authorDTO) throws NotFoundException {
@@ -51,6 +59,8 @@ public class AuthorController implements BaseControllerInterface<AuthorDTO,Long>
         return ResponseEntity.ok().body(new DefaultResponse(HttpStatus.OK.toString()));
     }
 
+    @Operation(summary = "delete author by author id. This deletes authors and details from authorbooks {authorbooks" +
+            " is the many to many child table between author and book} ")
     @DeleteMapping("/delete/{author_id}")
     @Override
     public ResponseEntity<?> delete(@PathVariable(value = "author_id") Long id) throws NotFoundException {
@@ -58,6 +68,7 @@ public class AuthorController implements BaseControllerInterface<AuthorDTO,Long>
         return ResponseEntity.ok().body(new DefaultResponse(HttpStatus.OK.toString()));
     }
 
+    @Operation(summary = "filter author by book count they have written")
     @GetMapping("/filterByBookCount/{count}")
     public ResponseEntity<?> filterAuthorByBookCount(@PathVariable(name = "count") int count) {
         return ResponseEntity.status(HttpStatus.OK).body(authorService.filterAuthorByBookCount(count));
